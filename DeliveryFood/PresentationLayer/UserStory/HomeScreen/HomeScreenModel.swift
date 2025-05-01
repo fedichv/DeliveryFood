4cae5005afffd9c921d2a4662f6bde1804943cc2
fedichv <fedichev06@bk.ru>
2025-05-01 14:03:59 +0300

    added folder home screen and file

---
+++ DeliveryFood/PresentationLayer/UserStory/HomeScreen/HomeScreenModel.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  DeliveryFood
+//
+//  Created by Владимир Федичев on 5/1/25.
+//
+
---
+++ DeliveryFood/PresentationLayer/UserStory/HomeScreen/HomeScreenView.swift
@@ -0,0 +1,7 @@
+//
+//  HomeScreenView.swift
+//  DeliveryFood
+//
+//  Created by Владимир Федичев on 5/1/25.
+//
+

698e235b2d08625250ede263fa084fdd3c00b70d
fedichv <fedichev06@bk.ru>
2025-05-01 14:04:50 +0300

    added folder tab bar

---
+++ DeliveryFood/PresentationLayer/UserStory/TabBar/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  DeliveryFood
+//
+//  Created by Владимир Федичев on 5/1/25.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}

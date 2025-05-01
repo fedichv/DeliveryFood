f9b80ae1031a4a95987292f5f35a3f2decc85628
fedichv <fedichev06@bk.ru>
2025-05-01 14:08:22 +0300

    added folder and file order

---
+++ DeliveryFood/PresentationLayer/UserStory/Order/OrderView.swift
@@ -0,0 +1,7 @@
+//
+//  OrderView.swift
+//  DeliveryFood
+//
+//  Created by Владимир Федичев on 5/1/25.
+//
+
---
+++ DeliveryFood/PresentationLayer/UserStory/Order/OrderViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  OrderViewModel.swift
+//  DeliveryFood
+//
+//  Created by Владимир Федичев on 5/1/25.
+//
+
